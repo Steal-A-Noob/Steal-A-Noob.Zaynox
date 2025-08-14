3e39261f59a6fbb6ed0031f282d7c168b9263b8b
Steal A Noob <loucasrobbe0@gmail.com>
2025-08-14 19:43:06 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,42 +1,55 @@
+/* Initialisation de Particles.js */
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 80, // Nombre de particules
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#00ffff" // 💠 Couleur cyan
+      "value": "#ff4500" // Couleur des particules (orange/rouge 🔥)
     },
     "shape": {
-      "type": "circle",
+      "type": "circle", // Cercle, triangle, edge, polygon
       "stroke": {
         "width": 0,
         "color": "#000000"
       }
     },
     "opacity": {
-      "value": 0.5,
-      "random": false
+      "value": 0.7,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.3,
+        "sync": false
+      }
     },
     "size": {
-      "value": 3,
-      "random": true
+      "value": 4,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 5,
+        "size_min": 0.5,
+        "sync": false
+      }
     },
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#00ffff", // 💠 Lignes cyan
+      "color": "#ff6347",
       "opacity": 0.4,
       "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 3,
+      "speed": 2,
       "direction": "none",
-      "random": false,
+      "random": true,
       "straight": false,
       "out_mode": "out",
       "bounce": false
@@ -47,19 +60,18 @@ particlesJS("particles-js", {
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "grab"
+        "mode": "repulse" // Les particules s'éloignent de la souris
       },
       "onclick": {
         "enable": true,
-        "mode": "push"
-      }
+        "mode": "push" // Clique pour ajouter des particules
+      },
+      "resize": true
     },
     "modes": {
-      "grab": {
-        "distance": 200,
-        "line_linked": {
-          "opacity": 0.8
-        }
+      "repulse": {
+        "distance": 100,
+        "duration": 0.4
       },
       "push": {
         "particles_nb": 4
