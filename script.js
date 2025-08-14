6078317572c5a9c418a80fcab95cbd8365fcb472
Zaynooxx <loucasrobbe0@gmail.com>
2025-08-14 15:47:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const priceText = document.getElementById('priceText');
 const popupBonus = document.getElementById('popupBonus');
 const popupClose = document.getElementById('popupClose');
 
+// Clique sur une image
 images.forEach(imgCard => {
   imgCard.addEventListener('click', () => {
     const title = imgCard.getAttribute('data-title');
@@ -15,22 +16,29 @@ images.forEach(imgCard => {
     const bonus = imgCard.getAttribute('data-bonus');
     const imgSrc = imgCard.getAttribute('data-img');
 
-    popupImage.src = imgSrc;
+    popupImage.src = imgSrc;  // Image du noob en haut
     popupTitle.textContent = title;
     popupRarity.textContent = `Rareté: ${rarity}`;
     priceText.textContent = `Prix: ${price}`;
     popupBonus.textContent = `Bonus: ${bonus}`;
 
+    // Supprimer toutes les classes glow popup
+    popup.classList.remove(
+      'popup-glow-Commun',
+      'popup-glow-Rare',
+      'popup-glow-UnCommun',
+      'popup-glow-Légendaire',
+      'popup-glow-Mythique'
+    );
+
+    // Ajouter glow selon rareté
+    popup.classList.add(`popup-glow-${rarity}`);
+
     popup.style.display = 'flex';
   });
 });
 
+// Fermer le popup
 popupClose.addEventListener('click', () => {
   popup.style.display = 'none';
-});
-
-window.addEventListener('click', (e) => {
-  if (e.target == popup) {
-    popup.style.display = 'none';
-  }
 });
