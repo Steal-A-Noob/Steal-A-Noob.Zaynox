86908628e6600d2f078270b7be9eb65ddb961b9e
Zaynooxx <loucasrobbe0@gmail.com>
2025-08-14 12:17:46 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -3,14 +3,40 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let particlesArray = [];
+let particles = [];
+for (let i = 0; i < 80; i++) {
+  particles.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    size: Math.random() * 3 + 1,
+    speedX: Math.random() * 2 - 1,
+    speedY: Math.random() * 2 - 1
+  });
+}
 
-class Particle {
-  constructor(x, y, size, color, speedY) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.color = color;
-    this.speedY = speedY;
-  }
-  update() { this.y -= this.speedY; if(this.y < 0)
+function drawParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'white';
+  particles.forEach(p => {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
+    ctx.fill();
+  });
+}
+
+function moveParticles() {
+  particles.forEach(p => {
+    p.x += p.speedX;
+    p.y += p.speedY;
+    if (p.x < 0 || p.x > canvas.width) p.speedX *= -1;
+    if (p.y < 0 || p.y > canvas.height) p.speedY *= -1;
+  });
+}
+
+function animateParticles() {
+  drawParticles();
+  moveParticles();
+  requestAnimationFrame(animateParticles);
+}
+
+animateParticles();
