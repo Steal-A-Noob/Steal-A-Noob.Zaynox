1724c214e4d6e3082c4f41f310c740143e039e34
Zaynooxx <loucasrobbe0@gmail.com>
2025-08-14 14:09:51 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,57 +1,79 @@
-/* Particles.js simple pour fond animé */
+/* particles.js - fond animé pour NoobFinder 🔥 */
 
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 80,
+      "value": 60, // nombre de particules
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#ffffff"
+      "value": "#ffffff" // couleur des particules (blanc)
     },
     "shape": {
-      "type": "circle"
+      "type": "circle", // forme des particules
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }
     },
     "opacity": {
-      "value": 0.5,
+      "value": 0.7, // transparence
       "random": true,
       "anim": {
         "enable": true,
         "speed": 1,
-        "opacity_min": 0.1,
+        "opacity_min": 0.2,
         "sync": false
       }
     },
     "size": {
-      "value": 3,
+      "value": 4, // taille des particules
       "random": true,
       "anim": {
-        "enable": false
+        "enable": true,
+        "speed": 3,
+        "size_min": 0.5,
+        "sync": false
       }
     },
     "line_linked": {
-      "enable": false
+      "enable": false // pas de lignes entre particules
     },
     "move": {
       "enable": true,
-      "speed": 1,
-      "direction": "top",
+      "speed": 2,
+      "direction": "none",
       "random": true,
       "straight": false,
-      "out_mode": "out"
+      "out_mode": "out",
+      "bounce": false
     }
   },
   "interactivity": {
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": false
+        "enable": true,
+        "mode": "grab" // effet quand on survole
       },
       "onclick": {
-        "enable": false
+        "enable": true,
+        "mode": "push" // ajoute des particules au clic
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 120,
+        "line_linked": {
+          "opacity": 0.4
+        }
+      },
+      "push": {
+        "particles_nb": 4
       }
     }
   },
