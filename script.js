b48dd5f12e7b17b8f96f84cf1d4330796fb35f72
Zaynooxx <loucasrobbe0@gmail.com>
2025-08-14 12:23:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,41 +7,9 @@ const priceText = document.getElementById("priceText");
 const popupBonus = document.getElementById("popupBonus");
 const popupClose = document.getElementById("popupClose");
 
-const imageCards = document.querySelectorAll(".image-card");
+const sortRarityBtn = document.getElementById("sortRarity");
+const sortPriceAscBtn = document.getElementById("sortPriceAsc");
+const sortPriceDescBtn = document.getElementById("sortPriceDesc");
+const imagesContainer = document.querySelector(".images-container");
 
-imageCards.forEach(card => {
-  card.addEventListener("click", () => {
-    const title = card.getAttribute("data-title");
-    const rarity = card.getAttribute("data-rarity");
-    const price = card.getAttribute("data-price");
-    const bonus = card.getAttribute("data-bonus");
-    const img = card.getAttribute("data-img");
-
-    popup.style.display = "block";
-    popupImage.src = img;
-    popupTitle.textContent = title;
-
-    let rarityColor = "#ccc";
-    if (rarity.toLowerCase() === "commun") rarityColor = "#aaa";
-    if (rarity.toLowerCase() === "rare") rarityColor = "skyblue";
-    if (rarity.toLowerCase() === "uncommun") rarityColor = "#00ff7f";
-    if (rarity.toLowerCase() === "légendaire") rarityColor = "orange";
-    if (rarity.toLowerCase() === "mythique") rarityColor = "hotpink";
-
-    popupRarity.innerHTML = `Rareté: <span style="color:${rarityColor}">${rarity}</span>`;
-    priceText.innerHTML = `Prix: <span style="color:lime">${price}</span>`;
-    popupBonus.innerHTML = `<span style="color:yellow">${bonus}</span>`;
-  });
-});
-
-popupClose.addEventListener("click", () => {
-  popup.style.display = "none";
-});
-
-searchBar.addEventListener("input", function() {
-  const value = searchBar.value.toLowerCase();
-  imageCards.forEach(card => {
-    const title = card.getAttribute("data-title").toLowerCase();
-    card.style.display = title.includes(value) ? "inline-block" : "none";
-  });
-});
+const
